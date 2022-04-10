8ce3c63150ad022f53e3144c33f348de7c0af3e0
Evgeniy Kolesnichenko <RovikHevik@gmail.com>
2022-04-10 22:43:21 +0300

    пофикшено - ура, товарищи!

--- DesktopTest/Logic/GrammarlyLogic.cs
+++ DesktopTest/Logic/GrammarlyLogic.cs
@@ -1,7 +1,11 @@
 ﻿using DesktopTest.Model;
 using FlaUI.Core;
+using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Tools;
 using FlaUI.UIA3;
 using System;
+using System.Linq;
+using System.Threading;
 
 namespace DesktopTest.Logic
 {
@@ -14,14 +18,25 @@ namespace DesktopTest.Logic
             Console.WriteLine($"{app.Name}: successfully, with processId: {app.ProcessId}");
         }
            
-        
         public bool IsValueCorrect(TextModel model)
         {
             using (var automation = new UIA3Automation())
             {
-                var screen = app.GetMainWindow(automation);
-                int count = 0;
-                Console.WriteLine($"{app.Name} count {screen.FindFirstDescendant(cf => cf.ByAutomationId("AlertsCounterLabel")).Name} errors");
+                AutomationElement errorLabel = null;
+                Thread.Sleep(TimeSpan.FromSeconds(3));                
+                var screen = app.GetAllTopLevelWindows(automation).First();
+                int count = -1;
+                for (int i = 0; i < 20; i++) 
+                { 
+                    Thread.Sleep(TimeSpan.FromSeconds(0.5));
+                    errorLabel = screen.FindFirstDescendant(cf => cf.ByAutomationId("AlertsCounterLabel"));
+                    if (errorLabel.Name == model.ErrorCount.ToString())
+                    {
+                        i = 20;
+                    }
+                }
+                int.TryParse(errorLabel.Name, out count);
+                Console.WriteLine($"{app.Name} count {count} errors");                
                 if (count == model.ErrorCount) return true;
                 else                           return false;
             }
--- DesktopTest/Program.cs
+++ DesktopTest/Program.cs
@@ -9,16 +9,17 @@ namespace DesktopTest
     internal class Program
     {
         const string PathToWord = @"C:\Program Files\Microsoft Office\root\Office16\WINWORD.EXE";
-        const string PathToGrammarly = @"C:\Users\HohloCit\AppData\Local\Grammarly\DesktopIntegrations\Grammarly.Desktop.exe";
+        const string PathToGrammarly = @"C:\Users\Fridoleen\AppData\Local\Grammarly\DesktopIntegrations\Grammarly.Desktop.exe";
         private static readonly TextModel modelToTest = new TextModel() { Text = "what r u know anout thes", ErrorCount = 2 };
+        private static readonly TextModel modelToTestTwo = new TextModel() { Text = "what r u know about this", ErrorCount = 0 };
         static void Main(string[] args)
         {
             GrammarlyLogic grammarlyLogic = new GrammarlyLogic(PathToGrammarly);
+            Thread.Sleep(TimeSpan.FromSeconds(2));
             ITextInput wordLogic = new WordLogic(PathToWord);
             //ITextInput wordLogic = new BrowserLogic();
             wordLogic.InputText(modelToTest);
-            Thread.Sleep(TimeSpan.FromSeconds(1));
-            grammarlyLogic.IsValueCorrect(modelToTest);
+            Console.WriteLine("Test result: " + grammarlyLogic.IsValueCorrect(modelToTest));
             Console.ReadKey();
         }
     }
